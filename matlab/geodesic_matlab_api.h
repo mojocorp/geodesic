48154dfbe5e90577393781f126e3b23cb4a1a37f
Morgan Leborgne <morgan.leborgne@zalando.de>
2019-01-31 13:41:27 +0100

    Make some port of the doc doxygen compatible.

--- matlab/geodesic_matlab_api.h
+++ matlab/geodesic_matlab_api.h
@@ -12,49 +12,46 @@
 extern "C"
 {
 #endif
-
-    GEODESIC_DLL_IMPORT long new_mesh(long num_points, // creates new mesh
+    //! creates new mesh
+    GEODESIC_DLL_IMPORT long new_mesh(long num_points,
                                       double* points,
                                       long num_triangles,
                                       long* triangles,
                                       long* num_edges,
                                       double** edges);
 
-    GEODESIC_DLL_IMPORT long new_algorithm(
-      long mesh_id, // creates a geodesic algorithm for a given mesh
-      long type,
-      long subdivision);
+    //! creates a geodesic algorithm for a given mesh
+    GEODESIC_DLL_IMPORT long new_algorithm(long mesh_id, long type, long subdivision);
 
     GEODESIC_DLL_IMPORT void delete_algorithm(long id);
 
-    GEODESIC_DLL_IMPORT void delete_mesh(long id); // delete mesh and all associated algorithms
+    //! delete mesh and all associated algorithms
+    GEODESIC_DLL_IMPORT void delete_mesh(long id);
 
+    //! compute distance field for given source points
     GEODESIC_DLL_IMPORT void propagate(
-      long algorithm_id, // compute distance field for given source points
+      long algorithm_id,
       double* source_points,
       long num_sources,
-      double* stop_points, // limitations on distance field propagation
+      double* stop_points, //!< limitations on distance field propagation
       long num_stop_points,
       double max_propagation_distance);
 
-    GEODESIC_DLL_IMPORT long trace_back(
-      long algorithm_id, // using procomputed distance field, compute a shortest path from
-                         // destination to the closest source
-      double* destination,
-      double** path);
+    //! using procomputed distance field, compute a shortest path from destination to the closest
+    //! source
+    GEODESIC_DLL_IMPORT long trace_back(long algorithm_id, double* destination, double** path);
 
-    GEODESIC_DLL_IMPORT long distance_and_source(
-      long algorithm_id, // quickly find what source this point belongs to and what is the distance
-                         // to this source
-      double* destination,
-      double* best_source_distance);
-
+    //! quickly find what source this point belongs to and what is the distance
+    //! to this source
+    GEODESIC_DLL_IMPORT long distance_and_source(long algorithm_id,
+                                                 double* destination,
+                                                 double* best_source_distance);
+    //! same idea as in the previous function
     GEODESIC_DLL_IMPORT long distance_and_source_for_all_vertices(
-      long algorithm_id,  // same idea as in the previous function
-      double** distances, // list distance/source info for all vertices of the mesh
+      long algorithm_id,
+      double** distances, //!< list distance/source info for all vertices of the mesh
       long** sources);
 
 #ifdef __cplusplus
 }
 #endif
-
